ff31d25c661435645d9eb345c934bbe99db23707
luke <2736230899@qq.com>
2023-05-10 11:19:36 +0800

    ENH: Handle not found words

--- merriam_api_parser/_requset_resopnse.py
+++ merriam_api_parser/_requset_resopnse.py
@@ -19,6 +19,11 @@ class RequestResponse:
         url: str = f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={self.api_key}"  # noqa: E501
         response: requests.Response = requests.get(url, timeout=5)
         json_res: list[dict[str, Any]] = json.loads(response.text)
-        # there should return all text write to md file
+        self._validate_json_response(json_res)
         return JsonParser(json_res[0]).get_md_text()
         # TODO: json_res[0] may be wrong
+
+    def _validate_json_response(self, json_data: list[dict[str, Any]]) -> None:
+        if not isinstance(json_data[0], dict):
+            msg: str = "Input words maybe not found"
+            raise TypeError(msg)
